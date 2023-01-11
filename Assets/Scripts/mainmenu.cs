7ab03fba4d13cac48f46af168af947676b5fb6e1
francorivaas <65259655+francorivaas@users.noreply.github.com>
2023-01-11 14:39:28 -0300

    a

---
+++ Assets/Scripts/mainmenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainmenu : MonoBehaviour
+{
+    [SerializeField] private GameObject[] buttons;
+
+    private float timeToSpawn = 6.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToSpawn -= Time.deltaTime;
+        if (timeToSpawn <= 0)
+        {
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].SetActive(true);
+            }
+        }
+
+        else
+        {
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].SetActive(false);
+            }
+        }
+    }
+}
