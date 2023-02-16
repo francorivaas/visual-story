ce924b01aa0043bb86864aabce7ab1a89fd51b83
francorivaas <65259655+francorivaas@users.noreply.github.com>
2023-02-16 10:46:47 -0300

    main menu edited

--- Assets/Scripts/mainmenu.cs
+++ Assets/Scripts/mainmenu.cs
@@ -2,27 +2,27 @@ using UnityEngine;
 
 public class mainmenu : MonoBehaviour
 {
-    [SerializeField] private GameObject[] buttons;
+    //[SerializeField] private GameObject[] buttons;
 
-    private float timeToSpawn = 6.0f;
+    //private float timeToSpawn = 6.0f;
 
-    void Update()
-    {
-        timeToSpawn -= Time.deltaTime;
-        if (timeToSpawn <= 0)
-        {
-            for (int i = 0; i < buttons.Length; i++)
-            {
-                buttons[i].SetActive(true);
-            }
-        }
+    //void Update()
+    //{
+    //    timeToSpawn -= Time.deltaTime;
+    //    if (timeToSpawn <= 0)
+    //    {
+    //        for (int i = 0; i < buttons.Length; i++)
+    //        {
+    //            buttons[i].SetActive(true);
+    //        }
+    //    }
 
-        else
-        {
-            for (int i = 0; i < buttons.Length; i++)
-            {
-                buttons[i].SetActive(false);
-            }
-        }
-    }
+    //    else
+    //    {
+    //        for (int i = 0; i < buttons.Length; i++)
+    //        {
+    //            buttons[i].SetActive(false);
+    //        }
+    //    }
+    //}
 }
